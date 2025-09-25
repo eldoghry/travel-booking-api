d518a69b37f6894c745c70f5ee37dcf48af808fb
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-09-25 18:30:18 +0300

    setup dto validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ENV_CONFIG, THROTTLE_CONFIG, TYPEORM_CONFIG } from './config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(ENV_CONFIG),
     ThrottlerModule.forRoot(THROTTLE_CONFIG),
     TypeOrmModule.forRootAsync(TYPEORM_CONFIG),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
