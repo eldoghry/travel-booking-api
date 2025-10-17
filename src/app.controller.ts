b421dac486c9fea1a49a42747ca1a3eaccb629fd
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-10-17 22:57:18 +0300

    chore: add exception filters

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Get, LoggerService } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  LoggerService,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { AuthenticatedUser, Roles, Unprotected } from 'nest-keycloak-connect';
