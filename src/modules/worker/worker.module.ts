66348b25d92b919785ed8aca6eb3e7503d0382af
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-11-13 20:26:18 +0200

    cleanup

--- src/modules/worker/worker.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoreModule } from '../core/core.module';
-import { EmailConsumer } from './consumers/email.consumer';
-
-@Module({
-  imports: [CoreModule],
-  providers: [EmailConsumer],
-})
-export class WorkerModule {}
