473e009bb4b0761e8ef7d9302da9398e8c7ee04a
Randa Magdy <randamagdy123@gmail.com>
2025-12-07 10:29:53 +0300

    fix: remove capturePayment method from payment strategy interface

--- src/modules/payment/interfaces/payment-strategy.interface.ts
+++ src/modules/payment/interfaces/payment-strategy.interface.ts
@@ -2,6 +2,5 @@ import { CreatePaymentDto } from '../dto/create-payment.dto';
 
 export interface PaymentStrategy {
   initiatePayment(body: CreatePaymentDto): Promise<any>;
-  capturePayment(body: any): Promise<any>;
   handleWebhook(req: Request): Promise<any>;
 }
