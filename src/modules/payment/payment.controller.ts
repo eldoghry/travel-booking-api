ff53c961c727f58821eef8ff695a578db77852bf
Randa Magdy <randamagdy123@gmail.com>
2025-12-12 17:16:19 +0300

    refactor(payment-controller): update handlePayPalWebhooks to process handled events and log unhandled event types

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Logger, Post, Req } from "@nestjs/common";
 import { PaymentService } from "./services/payment.service";
 import { CreatePaymentDto } from "./dto/create-payment.dto";
 import { Public } from "nest-keycloak-connect";
+import { PayPalEventType } from "./enums/paypal.enum";
+import { PayPalWebhookEvent } from "./interfaces/paypal.interface";
+import { PaymentProvider } from "./enums/payment-methods.enum";
 
 @Public()
 @Controller("payments")
 export class PaymentController {
     constructor(private readonly paymentService: PaymentService) { }
+    private readonly logger = new Logger(PaymentController.name);
 
     @Post('process')
     async initiatePayment(@Body() body: CreatePaymentDto) {
@@ -14,12 +18,18 @@ export class PaymentController {
     }
 
     @Post('webhooks/paypal')
-    async handlePayPalWebhooks(@Req() req: Request) {
-        return this.paymentService.handleWebhook(req, 'paypal');
+    async handlePayPalWebhooks(@Req() req: Request & { body: PayPalWebhookEvent }) {
+        const event = req.body
+        const eventType = event?.event_type as PayPalEventType
+        if (!Object.values(PayPalEventType).includes(eventType)) {
+            this.logger.log(`Received PayPal unhandled webhook event: ${eventType}`);
+        } else {
+            return this.paymentService.handleWebhook(req, PaymentProvider.PAYPAL);
+        }
     }
 
     @Post('webhooks/stripe')
     async handleStripeWebhooks(@Req() req: Request) {
-        return this.paymentService.handleWebhook(req, 'stripe');
+        return this.paymentService.handleWebhook(req, PaymentProvider.STRIPE);
     }
 }
